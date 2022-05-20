d9809a67f3f1e39fd26b81be27349196dcea992e
oriolli <mar.orioli@hotmail.com>
2022-05-20 15:03:50 -0300

    Programa contador de números de forma crescente e decrescente

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,36 @@
+function contar() {
+  let ini = document.getElementById('txti');
+  let fim = document.getElementById('txtf');
+  let passo = document.getElementById('txtp');
+  let res = document.getElementById('res');
+
+  if (
+    ini.value.length == 0 ||
+    fim.value.length == 0 ||
+    passo.value.length == 0
+  ) {
+    window.alert('[ERRO] Faltam dados!');
+    res.innerHTML = 'Impossível contar!';
+  } else {
+    res.innerHTML = 'Contando: <br>';
+    let i = Number(ini.value);
+    let f = Number(fim.value);
+    let p = Number(passo.value);
+    if (p <= 0) {
+      window.alert('Passo inválido! CONSIDERANDO PASSO 1 ');
+      P = 1;
+    }
+    if (i < f) {
+      // contagem crescente
+      for (let c = i; c <= f; c += p) {
+        res.innerHTML += `${c} \u{27A1}`;
+      }
+    } else {
+      // contagem regressiva
+      for (let c = i; c >= f; c -= p) {
+        res.innerHTML += `${c} \u{27A1}`;
+      }
+    }
+    res.innerHTML += '\u{1F3C1}';
+  }
+}
